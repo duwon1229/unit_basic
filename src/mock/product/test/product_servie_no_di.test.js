cc4688166a861b8c82aeecaeb46d5f3408a2bea8
duwon1229 <44364759+duwon1229@users.noreply.github.com>
2022-03-28 23:43:46 +0900

    mock, stub 차이

---
+++ src/mock/product/test/product_servie_no_di.test.js
@@ -0,0 +1,35 @@
+const ProductClient = require("../product_client");
+const ProductService = require("../product_service_no_di");
+
+// mock 남용 사례
+jest.mock("../product_client");
+
+describe("ProductService", () => {
+  const fetchItems = jest.fn(async () => [
+    { item: "Milk", available: true },
+    { item: "banana", available: false },
+  ]);
+  ProductClient.mockImplementation(() => {
+    return {
+      fetchItems,
+    };
+  });
+  let productService;
+
+  beforeEach(() => {
+    productService = new ProductService();
+    fetchItems.mockClear();
+    ProductClient.mockClear();
+  });
+
+  it("should filter out only availble items", async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(items.length).toBe(1);
+    expect(items).toEqual([{ item: "Milk", available: true }]);
+  });
+
+  it("test", async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(fetchItems).toHaveBeenCalledTimes(1);
+  });
+});
